56de225728862781606999cad66ebbd2f198c1fa
Adam Craggs <agcraggs.github@gmail.com>
2021-11-20 13:31:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # Directory that has been already been created by cloud-config-drupal.yaml
+chown ubuntu /run/cloud-config-drupal/install
 cd /run/cloud-config-drupal/install
+# Setup
 mkdir /run/cloud-config-drupal/setup
+chown ubuntu /run/cloud-config-drupal/setup
 cd /run/cloud-config-drupal/setup
 mkdir /home/ubuntu/bin
-sudo usermod --append --groups www-data ubuntu
+chown ubuntu /home/ubuntu/bin
+usermod --append --groups www-data ubuntu
 wget --output-document=composer-setup.php https://getcomposer.org/installer
 php composer-setup.php --install-dir="/home/ubuntu/bin" --filename=composer --quiet
