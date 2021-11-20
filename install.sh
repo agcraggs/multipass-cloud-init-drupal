280b1d8a99cb513641d93237d1e8b1acfb955457
Adam Craggs <agcraggs.github@gmail.com>
2021-11-20 19:17:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ chown --recursive ubuntu /run/cloud-config-drupal
 # Assume user is ubuntu
 mkdir /home/ubuntu/cloud-config-drupal
 mkdir /home/ubuntu/cloud-config-drupal/install
-cp /run/cloud-config-drupal/install/user-install.sh /home/ubuntu/cloud-config-drupal/install
+cp /run/cloud-config-drupal/install/install-user.sh /home/ubuntu/cloud-config-drupal/install/
 chown --recursive ubuntu /home/ubuntu/cloud-config-drupal
 # Add install script to the ubuntu user .profile file so it can run on login
 sed -i -e '$a\\n#multipass-cloud-init-drupal\n$HOME/cloud-config-drupal/install/install-user.sh\n' /home/ubuntu/.profile
