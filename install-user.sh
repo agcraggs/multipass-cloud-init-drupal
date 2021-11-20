b2aa4e857d98596c3ac1feb76d9338a5b222e40e
Adam Craggs <agcraggs.github@gmail.com>
2021-11-20 19:35:32 +0000

    Update install-user.sh

--- install-user.sh
+++ install-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -f "$HOME/cloud-config-drupal/.cloud-config-drupal" ]; then
+if [ ! -f "$HOME/cloud-config-drupal/.cloud-config-drupal" ]; then
   if [ -d "$HOME/cloud-config-drupal/install" ] ; then
     # Ready to install
     echo Starting: multipass-cloud-init-drupal
