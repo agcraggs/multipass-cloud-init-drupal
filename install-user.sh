f8d9d5f371bd8f5c91f00dec5af6e65a8715b59d
Adam Craggs <agcraggs.github@gmail.com>
2021-11-20 18:56:09 +0000

    Update to try and resolve broken install approach

---
+++ install-user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -f "$HOME/cloud-config-drupal/.cloud-config-drupal" ]; then
+  if [ -d "$HOME/cloud-config-drupal/install" ] ; then
+    # Ready to install
+    echo Starting: multipass-cloud-init-drupal
+    echo Finishing: multipass-cloud-init-drupal
+  fi
+# Install complete
+touch "$HOME/cloud-config-drupal/.cloud-config-drupal"
+fi
--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-# Directory that has been already been created by cloud-config-drupal.yaml
-chown ubuntu /run/cloud-config-drupal/install
-cd /run/cloud-config-drupal/install
-# Setup
-mkdir /run/cloud-config-drupal/setup
-chown ubuntu /run/cloud-config-drupal/setup
-cd /run/cloud-config-drupal/setup
-mkdir /home/ubuntu/bin
-chown ubuntu /home/ubuntu/bin
-usermod --append --groups www-data ubuntu
-wget --output-document=composer-setup.php https://getcomposer.org/installer
-php composer-setup.php --install-dir="/home/ubuntu/bin" --filename=composer --quiet
+# Directory has been already been created by cloud-config-drupal.yaml
+chown --recursive ubuntu /run/cloud-config-drupal
+# Assume user is ubuntu
+mkdir /home/ubuntu/cloud-config-drupal
+mkdir /home/ubuntu/cloud-config-drupal/install
+cp /run/cloud-config-drupal/install/user-install.sh /home/ubuntu/cloud-config-drupal/install
+chown --recursive ubuntu /home/ubuntu/cloud-config-drupal
+# Add install script to the ubuntu user .profile file so it can run on login
+sed -i -e '$a\\n#multipass-cloud-init-drupal\n$HOME/cloud-config-drupal/install-user.sh\n' /home/ubuntu/.profile
